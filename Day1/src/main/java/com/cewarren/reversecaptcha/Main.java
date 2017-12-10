b9ac02dc808f1e0bedbe00466d48e6bcdc7bfca0
cewarren <cewarren2@gmail.com>
2017-12-10 15:12:31 -0500

    Advent of Code - Day 1 Part 2 Complete - Java

--- Day1/src/main/java/com/cewarren/reversecaptcha/Main.java
+++ Day1/src/main/java/com/cewarren/reversecaptcha/Main.java
@@ -12,25 +12,33 @@ public class Main {
 		ArrayList<Integer> accessCode = new ArrayList<Integer>();
 	    int result = 0;
 	    
+	    String test = Main.class.getResource("resources/input.txt").toString();
+	    System.out.println(test);
+	    
 	    String inputFile = new String(
 	    		Files.readAllBytes(
 	    				Paths.get(
-	    						URI.create(Main.class.getResource("/resources/input.txt").toString()))));
+	    						URI.create(Main.class.getResource("resources/input.txt").toString()))));
 	    
 	    
 	    ArrayList<String> strArray = new ArrayList<String>(Arrays.asList(inputFile.split("")));
 	    strArray.removeAll(Arrays.asList(null, "\n"));
 	    
-	    System.out.println("String Array: \n" + strArray);
+	    //System.out.println("String Array: \n" + strArray);
 	    
 	    accessCode = getIntegerArray(strArray);
 	    
-	    System.out.println("Int Array: \n" + accessCode.toString());
+	    //System.out.println("Int Array: \n" + accessCode.toString());
 	    result = getSum(accessCode);
 	    
 	    System.out.println("Result: \n" + String.valueOf(result));
 	    
+	    result = 0;
+	    result = getSumLongCompare(accessCode);
 	    
+	    System.out.println("Result part 2: " + String.valueOf(result));
+	    
+	    System.exit(0);
 	}
 	
 	private static ArrayList<Integer> getIntegerArray(ArrayList<String> strArray){
@@ -62,6 +70,36 @@ public class Main {
 				}
 			}
 		}
+		
+		return sum;
+	}
+	
+	private static int getSumLongCompare(ArrayList<Integer> intArray) {
+		int sum = 0;
+		int arraySize = intArray.size();
+		int maxIndex = arraySize - 1;
+		int comparedIndex = arraySize / 2;
+
+		//Compare the current digit with the digit halfway around the array. if they match,
+		//it can be added to the sum. if the index is higher than the array size, loop around to the beginning.
+		
+		for(int i = 0; i < arraySize; i++) {
+			int currVal = intArray.get(i);
+			
+			if(comparedIndex >= arraySize) {
+				int wrappedIndex = comparedIndex - arraySize;
+				if(currVal == intArray.get(wrappedIndex)) {
+					sum += currVal;
+				}
+			} else {
+				if(currVal == intArray.get(comparedIndex)) {
+					sum += currVal;
+				}
+			}
+			
+			comparedIndex += 1;
+		}
+		
 		
 		return sum;
 	}
